b8308246e338ff5ff941e270f6d3d5b8aec1bfce
Shiv Shankar <shiv@pobox.com>
2013-08-28 10:40:14 +1000

    Symlink configuration for db and additional libraries.

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,7 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  config.vm.synced_folder "config/", "/usr/local/etc/", ownder: "root", group: "root"
   config.vm.provision :shell, :path => "./setup/deb-deps.sh"
   config.vm.provision :shell, :path => "./setup/fetch-sources.sh"
   config.vm.provision :shell, :path => "./setup/postgis-install.sh"
@@ -14,4 +15,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.provision :shell, :path => "./setup/node-deps.sh"
   config.vm.provision :shell, :path => "./setup/ruby-deps.sh"
   config.vm.provision :shell, :path => "./setup/cartodb-setup.sh"
+  config.vm.network :forwarded_port, guest: 3000, host: 3000
 end
--- setup/cartodb-setup.sh
+++ setup/cartodb-setup.sh
@@ -2,3 +2,28 @@
 
 cd /usr/local/src/cartodb
 bundle install
+
+mv config/app_config.yml.sample config/app_config.yml
+mv config/database.yml.sample config/database.yml
+
+# get postgres to drop all security
+mv /etc/postgresql/9.1/main/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf.original
+ln -s /usr/local/etc/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
+
+/etc/init.d/postgresql restart
+
+export USER=monkey
+export PASSWORD=monkey
+export ADMIN_PASSWORD=monkey
+export EMAIL=monkey@example.com
+
+echo "127.0.0.1 ${USER}.localhost.lan" | sudo tee -a /etc/hosts
+
+bundle exec rake cartodb:db:setup SUBDOMAIN="${USER}" \
+  PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" \
+  EMAIL="${EMAIL}"
+
+ln -s /usr/local/etc/cartodb.development.js /usr/local/src/CartoDB-SQL-API/config/environments/development.js
+ln -s /usr/local/etc/windshaft.development.js /usr/local/src/Windshaft-cartodb/config/environments/development.js
+
+/etc/init.d/redis-server stop
