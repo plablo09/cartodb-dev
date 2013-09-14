1769b346e3479fd723df1458e5c69affe738f771
Shiv Shankar <shiv@pobox.com>
2013-09-14 12:48:52 +1000

    add delay for restart to complete, call all associated db tasks

--- setup/cartodb-setup.sh
+++ setup/cartodb-setup.sh
@@ -12,6 +12,8 @@ ln -s /usr/local/etc/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 
 /etc/init.d/postgresql restart
 
+sleep 5s
+
 export USER=monkey
 export PASSWORD=monkey
 export ADMIN_PASSWORD=monkey
@@ -19,9 +21,11 @@ export EMAIL=monkey@example.com
 
 echo "127.0.0.1 ${USER}.localhost.lan" | sudo tee -a /etc/hosts
 
-bundle exec rake cartodb:db:setup SUBDOMAIN="${USER}" \
-  PASSWORD="${PASSWORD}" ADMIN_PASSWORD="${ADMIN_PASSWORD}" \
-  EMAIL="${EMAIL}"
+bundle exec rake rake:db:create
+bundle exec rake rake:db:migrate
+bundle exec rake cartodb:db:create_user SUBDOMAIN="${USER}" PASSWORD="${PASSWORD}" EMAIL="${EMAIL}"
+bundle exec rake cartodb:db:create_publicuser
+bundle exec rake cartodb:db:create_importer_schema
 
 ln -s /usr/local/etc/cartodb.development.js /usr/local/src/CartoDB-SQL-API/config/environments/development.js
 ln -s /usr/local/etc/windshaft.development.js /usr/local/src/Windshaft-cartodb/config/environments/development.js
