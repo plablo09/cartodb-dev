e86acd37647dd4879de8dda66ba447f4820b7fd5
Shiv Shankar <shiv@pobox.com>
2013-09-14 12:41:36 +1000

    add osgeo deps to fix uploads

--- setup/deb-deps.sh
+++ setup/deb-deps.sh
@@ -14,6 +14,7 @@ add-apt-repository -y ppa:mapnik/v2.1.0
 add-apt-repository -y ppa:cartodb/nodejs
 add-apt-repository -y ppa:cartodb/redis
 add-apt-repository -y ppa:cartodb/postgresql
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 
 apt-get update
 
@@ -21,5 +22,5 @@ apt-get install -y make unp zip libgeos-c1 libgeos-dev gdal-bin libgdal1-dev lib
   python-simplejson libjson0-dev proj-bin proj-data libproj-dev postgresql-9.1 \
   postgresql-client-9.1 postgresql-contrib-9.1 postgresql-server-dev-9.1 \
   postgresql-plpython-9.1 ruby1.9.1 ruby1.9.1-dev nodejs npm redis-server libmapnik-dev \
-  mapnik-utils python-mapnik git python-setuptools
+  mapnik-utils python-mapnik git python-setuptools python-gdal gdal-bin libgdal1-dev
 
