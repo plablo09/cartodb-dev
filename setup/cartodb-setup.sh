8627bd6c6ae6c9490cb74a48de2e64468c334124
Steve Bennett <stevage@gmail.com>
2014-04-17 23:47:07 +1000

    Fixing

--- setup/cartodb-setup.sh
+++ setup/cartodb-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+source settings
 cd /usr/local/src/cartodb
 bundle install
 
@@ -10,17 +10,26 @@ mv config/database.yml.sample config/database.yml
 #mv /etc/postgresql/9.1/main/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf.original
 #ln -s /usr/local/etc/pg_hba.conf /etc/postgresql/9.1/main/pg_hba.conf
 
-cd /etc/postgresql/9.1/main
-# sudo vim pg_hba.conf
-
 # Replaces even commented out lines which isn't ideal...
+cd /etc/postgresql/9.1/main
 sudo perl -i.bak -p -e's/(md5|peer)$/trust/g' pg_hba.conf
+sudo service postgresql reload
 
-
-/etc/init.d/postgresql restart
-redis-server &
+pgrep redis-server || redis-server &
 
 sleep 5s
+
+# The included config files are way out of date and will cause you very much pain
+#ln -s /usr/local/etc/cartodb.development.js /usr/local/src/CartoDB-SQL-API/config/environments/development.js
+#ln -s /usr/local/etc/windshaft.development.js /usr/local/src/Windshaft-cartodb/config/environments/development.js
+# Instead, just patch the current ones:
+cd /usr/local/src/
+cp CartoDB-SQL-API/config/environments/development.js.example CartoDB-SQL-API/config/environments/development.js
+perl -p -i -e 's/^(module.exports.node_host).*$/\1 = '"''/g" /usr/local/src/CartoDB-SQL-API/config/environments/development.js
+
+cp Windshaft-cartodb/config/environments/development.js.example Windshaft-cartodb/config/environments/development.js
+perl -p -i -e 's/^(    ,host:).*$/\1 '"''/g" /usr/local/src/Windshaft-cartodb/config/environments/development.js
+perl -p -i -e 's/^(    ,mapnik_version:).*$/\1 '"'2.1.1'/g" /usr/local/src/Windshaft-cartodb/config/environments/development.js
 
 
 echo "127.0.0.1 ${USER}.localhost.lan" | sudo tee -a /etc/hosts
@@ -41,18 +50,7 @@ bundle exec rake cartodb:db:set_user_private_tables_enabled["${SUBDOMAIN}",'true
 bundle exec rake cartodb:db:set_user_account_type["${SUBDOMAIN}",'[DEDICATED]']
 
 
-# The included config files are way out of date and will cause you very much pain
-#ln -s /usr/local/etc/cartodb.development.js /usr/local/src/CartoDB-SQL-API/config/environments/development.js
-#ln -s /usr/local/etc/windshaft.development.js /usr/local/src/Windshaft-cartodb/config/environments/development.js
 
-# Instead, just patch the current ones:
-cd /usr/local/src/
-cp CartoDB-SQL-API/config/environments/development.js.example CartoDB-SQL-API/config/environments/development.js
-perl -p -i -e 's/^(module.exports.node_host).*$/\1 = '"''/g" /usr/local/src/CartoDB-SQL-API/config/environments/development.js
-
-cp Windshaft-cartodb/config/environments/development.js.example Windshaft-cartodb/config/environments/development.js
-perl -p -i -e 's/^(    ,host:).*$/\1 '"''/g" /usr/local/src/Windshaft-cartodb/config/environments/development.js
-perl -p -i -e 's/^(    ,mapnik_version:).*$/\1 '"'2.1.1'/g" /usr/local/src/Windshaft-cartodb/config/environments/development.js
 
 # Otherwise a stack overflow error.
 mkdir -p /usr/local/src/cartodb/logs
